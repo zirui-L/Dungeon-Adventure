b1c5325512a5e204da361cfe79ebb5f54042e2b8
Zhenbo Zhang <zb.zhang.general@gmail.com>
2023-07-18 22:10:09 +1000

    Finished task1, section a), used strategy pattern to reduce code repetition in enemy eneties

---
+++ src/main/java/dungeonmania/entities/enemies/AwayMove.java
@@ -0,0 +1,42 @@
+package dungeonmania.entities.enemies;
+
+import dungeonmania.Game;
+import dungeonmania.map.GameMap;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class AwayMove implements MoveMethod {
+    public void nextPosition(Enemy enemy, Game game) {
+        GameMap map = game.getMap();
+        Position enemyPosition = enemy.getPosition();
+
+        Position plrDiff = Position.calculatePositionBetween(map.getPlayer().getPosition(), enemyPosition);
+
+        Position moveX = (plrDiff.getX() >= 0) ? Position.translateBy(enemyPosition, Direction.RIGHT)
+                : Position.translateBy(enemyPosition, Direction.LEFT);
+        Position moveY = (plrDiff.getY() >= 0) ? Position.translateBy(enemyPosition, Direction.UP)
+                : Position.translateBy(enemyPosition, Direction.DOWN);
+        Position offset = enemyPosition;
+        if (plrDiff.getY() == 0 && map.canMoveTo(enemy, moveX))
+            offset = moveX;
+        else if (plrDiff.getX() == 0 && map.canMoveTo(enemy, moveY))
+            offset = moveY;
+        else if (Math.abs(plrDiff.getX()) >= Math.abs(plrDiff.getY())) {
+            if (map.canMoveTo(enemy, moveX))
+                offset = moveX;
+            else if (map.canMoveTo(enemy, moveY))
+                offset = moveY;
+            else
+                offset = enemyPosition;
+        } else {
+            if (map.canMoveTo(enemy, moveY))
+                offset = moveY;
+            else if (map.canMoveTo(enemy, moveX))
+                offset = moveX;
+            else
+                offset = enemyPosition;
+        }
+
+        map.moveTo(enemy, offset);
+    }
+}
---
+++ src/main/java/dungeonmania/entities/enemies/RandomMove.java
@@ -0,0 +1,27 @@
+package dungeonmania.entities.enemies;
+
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+import dungeonmania.Game;
+import dungeonmania.map.GameMap;
+import dungeonmania.util.Position;
+
+public class RandomMove implements MoveMethod {
+    private Random randGen = new Random();
+
+    public void nextPosition(Enemy enemy, Game game) {
+        Position nextPos;
+        GameMap map = game.getMap();
+        List<Position> pos = enemy.getPosition().getCardinallyAdjacentPositions();
+        pos = pos.stream().filter(p -> map.canMoveTo(enemy, p)).collect(Collectors.toList());
+        if (pos.size() == 0) {
+            nextPos = enemy.getPosition();
+        } else {
+            nextPos = pos.get(randGen.nextInt(pos.size()));
+        }
+
+        map.moveTo(enemy, nextPos);
+    }
+}
